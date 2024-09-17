8f515830b4c67c6271c254fffde41a47f6023f50
nathanogaga118 <106948598+nathanogaga118@users.noreply.github.com>
2024-09-17 04:26:33 -0700

    Add files via upload
    first commit

---
+++ Third.java
@@ -0,0 +1,10 @@
+//CLASS NAME AND FILE NAME ARE THE SAME -Comments
+public class Third{
+    // MAIN METHOD
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Welcome to java class...");
+   }
+    // Kami24Git
+   
+
